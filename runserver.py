0a56397b94096b768870871d226bf3694cfa3a2e
ab.gupta1 <abhishake.gupta@geminisolutions.in>
2019-07-04 17:43:52 +0530

    added Reporting api

--- runserver.py
+++ runserver.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 
-from api import bp_product, bp_customer, bp_quotation, bp_store, bp_trader
+from api import bp_product, bp_customer, bp_quotation, bp_store, bp_trader, bp_report
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -10,6 +11,9 @@ app.register_blueprint(bp_product, url_prefix="/product")
 app.register_blueprint(bp_customer, url_prefix="/customer")
 app.register_blueprint(bp_store, url_prefix="/store")
 app.register_blueprint(bp_trader, url_prefix="/trader")
+app.register_blueprint(bp_report, url_prefix="/report")
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001, debug=True)
