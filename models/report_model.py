3d7e41635bb28e262c1d8e29c90d2ae5799b4f6a
ab.gupta1 <abhishake.gupta@geminisolutions.in>
2019-07-04 17:39:34 +0530

    Added model for reporting

---
+++ models/report_model.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+
+from boto3.dynamodb.conditions import Key, Attr
+from decimal import Decimal
+from models.retail_model import RetailModel
+from utils.generic_utils import get_logger
+
+TIMESTAMP = datetime.now
+logger = get_logger(__name__)
+
+
+class ProductModel(RetailModel):
+    def __init__(self):
+        super(ProductModel, self).__init__()
+        self.table = 'Product'
+
+    def generate_new_product_id(self):
+        """
+        get the number of pk that starts with "product"
+        :return:
+        """
+        _id = self.get_num_records("PRODUCT") + 1
+        print(_id)
+        return _id
+
+    def insert(self, product):
+        category_id = product.get('category_id')
+        description = product.get('description')
+        is_active = product.get("is_active", 1)
+        product_id = self.generate_new_product_id()
+        product_name = product.get('product_name')
+        sell_price = Decimal(product.get('sell_price'))
+        serial_no = product.get('serial_no')
+        supplier_id = product.get('supplier_id')
+        units_in_stock = int(product.get('units_in_stock', 0))
+        unit_price = Decimal(product.get('unit_price'))
+
+        item = {
+            "pk": f"{'products'}#{product_id}",
+            "sk": f"PRODUCT",
+            "data": f"{product_name}#{category_id}#{serial_no}#{is_active}"
+        }
+
+        item.update(product)
+        # TODO: Check if the same item already exists
+        already_existing_id = self.if_item_already_exists(item, sk='PRODUCT')
+        if already_existing_id:
+            return already_existing_id
+        else:
+            if self.save(item):
+                return product_id
+
+    def search_by_product_id(self, product_id):
+        val = f"{'products'}#{product_id}"
+        logger.info(f"Searching the product ID: {product_id} ...")
+        return self.get_by_partition_key(val)
+
+    def search_by_name(self, product_name):
+        # val = f"{product_name}"
+        logger.info(f"Searching the PRODUCT by Name: {product_name} ...")
+        response = self.model.query(IndexName='gsi_1',
+                                    KeyConditionExpression=Key('sk').eq('PRODUCT') & Key('data').begins_with(
+                                        product_name))
+        data = response['Items']
+        return data
+
+    def get_recent_products(self, limit):
+        print(limit)
+        response = self.model.query(IndexName='gsi_1', KeyConditionExpression=Key('sk').eq('PRODUCT'), Limit=limit)
+        data = response['Items']
+        # data.sort(key=lambda item: int(item['pk'].split('#')[1]))
+        return data
+
+    def update_product_item(self, product_id, product):
+        print(self.get_by_partition_key(product_id))
+        product.pop('product_id')
+        product.pop('description')
+        product.pop('sell_price')
+        product.pop('unit_price')
+        key = {'pk': f"{'products'}#{product_id}", "sk": "PRODUCT"}
+        print("KEY : ", key)
+        attribute_updates = self.get_changed_elements(product_id, product)
+        # print("attribute_updates ", attribute_updates)
+
+        UpdateExpression = "SET "
+        ExpressionAttributeValues = {}
+
+        for k in attribute_updates.keys():
+            UpdateExpression += "{}=:{}, ".format(k, k)
+            ExpressionAttributeValues.update({
+                ":{}".format(k): str(attribute_updates.get(k))
+            })
+
+        self.update(key, UpdateExpression[:-2], ExpressionAttributeValues)
+
+    def update_quantity_in_stocks(self, product_id, quantity):
+        key = {'pk': f"{'products'}#{product_id}", "sk": "PRODUCT"}
+        self.search_by_product_id(product_id)
+        UpdateExpression = "SET unitsInStock=:unitsInStock"
+        ExpressionAttributeValues = {
+            ":unitsInStock": quantity
+        }
+        updated_data = self.update(key, UpdateExpression, ExpressionAttributeValues)
+        return updated_data["unitsInStock"]
+
+    def get_changed_elements(self, product_id, new_product):
+        ori_product = self.search_by_product_id(product_id)[0]
+        changes = {}
+        for key in new_product.keys():
+            if ori_product.get(key) and new_product.get(key) == ori_product.get(key):
+                changes[key] = ori_product.get(key)
+            else:
+                changes[key] = new_product.get(key)
+        # print("Changed : ", changes)
+        return changes
+
+    def search_by_serial_no(self, serialNo):
+        logger.info("Searching by Serial Number: %s" % serialNo)
+        response = self.model.query(IndexName='gsi_1',
+                                    KeyConditionExpression=Key('sk').eq('PRODUCT'),
+                                    FilterExpression=Attr('serialNo').eq(serialNo)
+                                    )
+        # print(response)
+        # print(len(response['Items']))
+        return (response['Items'])
+
+    def delete_product(self, product_id):
+        """
+        Mark the product as in active by SET is_active=0
+        :param product_id:
+        :return:
+        """
+        # productID = "products#{}".format(product_id)
+        key = {'pk': f"{'products'}#{product_id}", "sk": "PRODUCT"}
+        UpdateExpression = "SET is_active=:is_active"
+        ExpressionAttributeValues = {
+            ":is_active": 0
+        }
+        updated_data = self.update(key, UpdateExpression, ExpressionAttributeValues)
+        return product_id
+        # return self.delete(product_id, sk='PRODUCT')
