ce1498930c5c93294cab87c486d27b8d149b99cb
ab.gupta1 <abhishake.gupta@geminisolutions.in>
2019-07-04 17:44:45 +0530

    Update Validation forms for customer & product

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ bp_product = Blueprint("product_view", __name__)
 bp_quotation = Blueprint("quotation_view", __name__)
 bp_store = Blueprint("store_view", __name__)
 bp_trader = Blueprint("trader_view", __name__)
+bp_report = Blueprint("report_view", __name__)
 
 from forms.validate_forms import ValidateCustomer, ValidateProduct, ValidateQuotation, ValidateUser, ValidateStore, ValidateTrader
-from api.blueprints import customer, product, quotation, store, trader
+from api.blueprints import customer, product, quotation, store, trader, report
--- forms/validate_forms.py
+++ forms/validate_forms.py
@@ -36,69 +36,63 @@ class ValidateProduct(ValidatePayload):
 
     @staticmethod
     def __validate(product):
-        assert isinstance(product.get('name'), str)
+        assert isinstance(product.get('product_name'), str)
         assert isinstance(product.get('serial_no'), str)
         assert isinstance(product.get('description'), str)
-        assert isinstance(product.get('distributor'), str)
-        assert isinstance(product.get('category'), str)
-        assert isinstance(product.get('cost_price'), int)
-        assert isinstance(product.get('sell_price'), int)
-        assert isinstance(product.get('quantity'), int)
+        assert isinstance(product.get('supplier_id'), str)
+        assert isinstance(product.get('category_id'), str)
+        assert isinstance(product.get('unit_price'), int) or isinstance(product.get('unit_price'), float)
+        assert isinstance(product.get('sell_price'), int) or isinstance(product.get('sell_price'), float)
+        assert isinstance(product.get('units_in_stock'), int)
         assert isinstance(product.get('is_active'), int)
         return True
 
 
 class ValidateUser(ValidatePayload):
-    def __init__(self, customer=None, **kw):
+    def __init__(self, user=None, **kw):
         super(ValidateUser, self).__init__(**kw)
-        self.first_name = customer.get('first_name')
-        self.middle_name = customer.get('middle_name')
-        self.last_name = customer.get('last_name')
-        self.email = customer.get("email")
-        self.phone = customer.get("phone")  # self.get_phone(customer)
-        self.gender = customer.get("gender")
-        self.category = customer.get("category")
-        self.dob = customer.get("dob")
-        self.membership = customer.get("membership")
-        self.postcode = customer.get("postcode")
-        self.state = customer.get("state")
-        self.city = customer.get("city")
-        self.country = customer.get("country")
-        self.address = self.get_address(customer)
-
-    # @staticmethod
-    # def get_phone(customer):
-    #     if customer.get('phone_1') and customer.get('phone_2'):
-    #         phone = [customer.get('phone_1'), customer.get('phone_2')]
-    #     else:
-    #         phone = customer.get('phone_1') or customer.get('phone_2')
-    #     return phone
-
-    @staticmethod
-    def get_address(customer):
-        if customer.get('address_1') and customer.get('address_2'):
-            address = [customer.get('address_1'), customer.get('address_2')]
-        else:
-            address = customer.get('address_1') or customer.get('address_2')
-        return address
+        self.user = user
 
     def validate(self):
-        assert isinstance(self.first_name, str)
-        assert isinstance(self.middle_name, str)
-        assert isinstance(self.last_name, str)
-        assert isinstance(self.gender, str)
-        assert isinstance(self.phone, str)
-        assert isinstance(self.email, str)
-        assert isinstance(self.category, str)
-        assert isinstance(self.dob, str)
-        assert isinstance(self.membership, str)
-        assert isinstance(self.state, str)
-        assert isinstance(self.city, str)
-        assert isinstance(self.country, str)
-        assert isinstance(self.postcode, str)
-        print(self.address)
-        assert isinstance(self.address, str) or isinstance(self.address, list)
+        if self.user:
+            if isinstance(self.user, dict):
+                return self.__validate(self.user)
 
+    @staticmethod
+    def __validate(user):
+        first_name = user.get('first_name')
+        middle_name = user.get('middle_name',"#")
+        last_name = user.get('last_name')
+        email = user.get("email")
+        primary_phone = user.get("primary_phone")
+        secondary_phone= user.get("secondary_phone")
+        fax = user.get("fax")
+        gender = user.get("gender")
+        is_active = user.get("is_active")
+        membership = user.get("membership")
+        postal_code = user.get("postal_code")
+        state = user.get("state")
+        city = user.get("city")
+        country = user.get("country")
+        address = user.get("address")
+        contact_title = user.get("contact_title")
+
+        assert isinstance(first_name, str)
+        assert isinstance(middle_name, str)
+        assert isinstance(last_name, str)
+        assert isinstance(contact_title, str)
+        assert isinstance(is_active, bool)
+        assert isinstance(gender, str)
+        assert isinstance(primary_phone, str)
+        assert isinstance(secondary_phone, str)
+        assert isinstance(email, str)
+        assert isinstance(fax, str)
+        assert isinstance(membership, str)
+        assert isinstance(state, str)
+        assert isinstance(city, str)
+        assert isinstance(country, str)
+        assert isinstance(postal_code, str)
+        assert isinstance(address, str)
         return True
 
 
@@ -121,7 +115,7 @@ class ValidateQuotation(ValidatePayload):
             assert isinstance(product.get('quantity'), int)
             assert isinstance(product.get('quoted_price'), int)
             assert isinstance(product.get('discount'), int)
-            assert isinstance(product.get('sub_total'), int)
+            assert isinstance(product.get('sub_total'), float)
         return True
 
 
