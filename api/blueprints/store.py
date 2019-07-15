d885e6be0166f5ae46fa0ba74dfb1a1ef0409eb0
ab.gupta1 <abhishake.gupta@geminisolutions.in>
2019-07-15 18:53:44 +0530

    Commented fields & marshal_with decorator

--- api/blueprints/store.py
+++ api/blueprints/store.py
@@ -1,5 +1,7 @@
-from flask_restplus import Resource, Api, fields, abort
-from flask import request
+import simplejson as json
+from flask import request, Response
+from flask_restplus import Resource, Api, abort
+
 from api import bp_store, ValidateStore
 from models.store_model import StoreModel
 from utils.generic_utils import get_logger
@@ -11,53 +13,71 @@ api = Api(bp_store,
           description="Manage Stores",
           doc='/api/documentation')
 
-store_fields = api.model('storeRequestFields', {
-    'store_id': fields.Integer(),
-    'name': fields.String('Name of Store'),
-    'address': fields.String('Address of Store'),
-    'country': fields.String('Country of Store'),
-})
 
-store_res_model = api.model('storeResponse', {
-    'message': fields.String('SUCCESS'),
-    'store': fields.Nested(store_fields)
-})
-
-
-@api.route('/')
+#
+# store_fields = api.model('storeRequestFields', {
+#     'store_id': fields.Integer(),
+#     'name': fields.String('Name of Store'),
+#     'category': fields.String('Store Category'),
+#     'address': fields.String('Address of Store'),
+#     'country': fields.String('Country of Store'),
+# })
+#
+# store_res_model = api.model('storeResponse', {
+#     'message': fields.String('SUCCESS'),
+#     'store': fields.Nested(store_fields)
+# })
+#
+#
+# @api.route('/')
 class Store(Resource):
-    @api.response(401, "Unauthorized")
-    @api.response(500, "Internal Server Error")
-    @api.doc(params={'store_id': 'An ID'})
-    @api.marshal_with(store_res_model, envelope="data")
-    def get(self, store_id=None):
-        store_id = request.args.get('store_id')
+    # @api.response(401, "Unauthorized")
+    # @api.response(500, "Internal Server Error")
+    # @api.doc(params={'store_id': 'An ID'})
+    # @api.marshal_with(store_res_model, envelope="data")
+    def get(self):
+        store_id = request.args.get('store_id', '')
         print("Need to fetch data for Store ID: %s" % store_id)
         sm = StoreModel()
-        if store_id is not None:
-            store = sm.get_record_by_id(int(store_id))
-            if not store:
+        if store_id not in ['null', '', None]:
+            data = sm.search_by_store_id(store_id)
+            if not data:
                 abort(404, "Resource Not Found")
         else:
-            print("Store id absent")
-            store = sm.get_all_records()
+            logger.info("Fetching all stores")
+            data = sm.get_all_stores()
 
-        print(store)
-        data = dict(store=store)
-        logger.info("PAYLOAD SENT: %s" % data)
+        # data = dict(store=store)
+        payload = json.dumps(data)
+        logger.info("PAYLOAD SENT: %s" % payload)
         return data
 
-    @api.response(401, "Unauthorized")
-    @api.response(500, "Internal Server Error")
-    @api.response(201, "Resource successfully added")
-    @api.expect(store_fields)
+    # @api.response(401, "Unauthorized")
+    # @api.response(500, "Internal Server Error")
+    # @api.response(201, "Resource successfully added")
+    # @api.expect(store_fields)
     def post(self):
-        store = api.payload
-        print(store)
+        status = 201
+        if not request.json:
+            abort(status)
+        store = request.json
+
+        store_id = None
         vf = ValidateStore(store)
-        vf.validate()
-        sm = StoreModel(store)
-        sm.create()
-        data = dict(store_id=sm.store_id)
-        logger.info("PAYLOAD SENT: %s" % data)
-        return data
+        if vf.validate():
+            # single product
+            sm = StoreModel()
+            store_id = sm.insert(store)
+            message = "Store added Successfully"
+        else:
+            message = "Invalid Store details"
+
+        data = dict(store_id=store_id,
+                    message=message)
+
+        payload = json.dumps(data)
+        logger.info("PAYLOAD SENT: %s" % payload)
+        return Response(payload, status=status, mimetype="application/json")
+
+
+api.add_resource(Store, "/")
