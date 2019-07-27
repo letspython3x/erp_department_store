fbe9b06ec5a2070ce0c3a2163fdf585d7420e8e9
ab.gupta1 <abhishake.gupta@geminisolutions.in>
2019-07-28 01:07:07 +0530

    Update insert function to insert more values

--- models/store_model.py
+++ models/store_model.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from boto3.dynamodb.conditions import Key, Attr
+
+from boto3.dynamodb.conditions import Key
+
 from models.retail_model import RetailModel
 from utils.generic_utils import get_logger
 
@@ -9,13 +11,13 @@ logger = get_logger(__name__)
 
 class StoreModel(RetailModel):
     def __init__(self):
-        logger.info("Initializing Customer...")
+        logger.info("Initializing Store...")
         super(StoreModel, self).__init__()
         self.table = 'STORE'
 
     def generate_new_store_id(self):
         """
-        get the number of pk that starts with "STORE"
+        get the number of pk that starts with "store"
         :return:
         """
         _id = self.get_num_records(self.table) + 1
@@ -24,21 +26,20 @@ class StoreModel(RetailModel):
 
     def insert(self, store):
         store_id = self.generate_new_store_id()
-        store['store_id'] = store_id
         store_name = store.get('store_name')
         store_admin = store.get('store_admin')
-        address = store.get('address')
-        phone = store.get('phone')
         city = store.get('city')
         country = store.get('country')
         postal_code = store.get('postal_code')
+        address = store.get('address')
+        phone = store.get('phone')
 
         item = dict(
             pk=f"stores#{store_id}",
             sk="STORE",
-            data="store_name#category_id#store_admin"
+            data=f"{store_name}#{store_admin}#{country}#{city}",
+            store_id=store_id
         )
-
         item.update(store)
         already_existing_id = self.if_item_already_exists(item, sk='STORE')
         if already_existing_id:
@@ -52,27 +53,10 @@ class StoreModel(RetailModel):
         logger.info(f"Searching the Store by ID: {store_id}")
         return self.get_by_partition_key(val)
 
-
     def get_all_stores(self):
         response = self.model.query(
             IndexName='gsi_1',
-            KeyConditionExpression=Key('sk').eq('STORE') )
+            KeyConditionExpression=Key('sk').eq('STORE'))
         data = response['Items']
         data.sort(key=lambda item: item.get('store_name'))
         return data
-
-
-    @staticmethod
-    def reformat(store):
-        if isinstance(store, list):
-            for p in store:
-                p.pop("pk")
-                p.pop("sk")
-                p.pop("data")
-        elif isinstance(store, dict):
-            store.pop("pk")
-            store.pop("sk")
-            store.pop("data")
-        else:
-            return store
-        return store
--- models/trader_model.py
+++ models/trader_model.py
@@ -1,46 +1,68 @@
 from datetime import datetime
 
-from models.base_model import BaseModel
+from boto3.dynamodb.conditions import Key
+
+from models.retail_model import RetailModel
 from utils.generic_utils import get_logger
 
 TIMESTAMP = datetime.now
 logger = get_logger(__name__)
 
 
-class TraderModel(BaseModel):
-    def __init__(self, trader=None):
-        logger.info("Initializing Customer...")
-        super(TraderModel, self).__init__(table_name='Trader')
-        if trader:
-            print("Fetching Customer details...")
-            self.name = trader.get('name')
-            self.address = trader.get('address')
-            self.country = trader.get('country')
+class TraderModel(RetailModel):
+    def __init__(self):
+        logger.info("Initializing Trader...")
+        super(TraderModel, self).__init__()
+        self.table = 'TRADER'
 
-        self.table_name = 'Trader'
-        self.pk_key = 'trader_id'
-        self.trader_id = 0
+    def generate_new_trader_id(self):
+        """
+        get the number of pk that starts with "store"
+        :return:
+        """
+        _id = self.get_num_records(self.table) + 1
+        print(_id)
+        return _id
 
-    def create(self):
-        trader_id = self.db.get_total_records_count() + 1
-        logger.info(f"Saving the New Trader, ID: %s" % trader_id)
-        record = dict(
-            trader_id=trader_id,
-            name=self.name,
-            address=self.address,
-            country=self.country
-        )
-        is_added = self.db.add_new_records(record, pk_key=self.pk_key)
-        if is_added:
-            logger.info(f"New Trader Saved successfully; ID:{trader_id}")
-            self.trader_id = trader_id
+    def insert(self, trader):
+        trader_id = self.generate_new_trader_id()
+        primary_phone = trader.get("primary_phone")
+        email = trader.get("email")
+        country = trader.get("country", '')
+        first_name = trader.get('first_name')
+        middle_name = trader.get('middle_name', "#")
+        last_name = trader.get('last_name')
+        address = trader.get('address')
+        phone = trader.get('phone')
+        city = trader.get('city')
+        postal_code = trader.get('postal_code')
+        secondary_phone = trader.get("secondary_phone")
+        fax = trader.get("fax")
+        contact_title = trader.get("contact_title")
 
-    def get_record_by_id(self, _id):
-        record = self.search_by_id(pk_key=self.pk_key, pk_val=_id)
-        if record:
-            return record[0]
+        item = dict(
+            pk=f"traders#{trader_id}",
+            sk="TRADER",
+            data=f"{primary_phone}#{email}#{country}#{city}",
+            trader_id=trader_id)
 
-    def get_all_records(self):
-        print("fetching all records")
-        records = self.fetch_all(pk_key=self.pk_key)
-        return records
+        item.update(trader)
+        already_existing_id = self.if_item_already_exists(item, sk='TRADER')
+        if already_existing_id:
+            return already_existing_id
+        else:
+            if self.save(item):
+                return trader_id
+
+    def search_by_trader_id(self, trader_id):
+        val = f"{'traders'}#{trader_id}"
+        logger.info(f"Searching the TRADER by ID: {trader_id}")
+        return self.get_by_partition_key(val)
+
+    def get_all_traders(self):
+        response = self.model.query(
+            IndexName='gsi_1',
+            KeyConditionExpression=Key('sk').eq('TRADER'))
+        data = response['Items']
+        data.sort(key=lambda item: item.get('trader_id'))
+        return data
